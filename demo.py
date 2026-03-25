fca1ab561111a5bf4b2bcecdb7fbceb298611199
YuvrajGaykhe <yuvrajgaykhe.17@gmail.com>
2026-03-25 09:55:17 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,11 @@
+import demo_module
+demo_module.greet("Yuvraj")
+result = demo_module.add(5, 3)
+print(f"The result of addition is: {result}")
+
+import math
+
+print(f"The addition of 5 and 3 is: {math.add(5, 3)}")
+
+print(f"The square root of 16 is: {math.sqrt(16)}")
+print(f"the factorial of 5 is: {math.factorial(5)}")
